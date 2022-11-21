67f6997c53ba7e30e80e558c124fced189d4325a
Parth <parthsm2001@gmail.com>
2022-11-21 13:35:08 +0530

    Add files via upload

---
+++ Packages.java
@@ -0,0 +1,58 @@
+package com.company;
+
+class A {
+    public int x = 5;
+    protected int y = 77;
+    int z = 19;
+    private int a = 6;
+
+    public void methA() {
+        System.out.println("\nAccessing the properties from the same class: ");
+        System.out.println("Public = " + x);
+        System.out.println("Protected = " + y);
+        System.out.println("Default = " + z);
+        System.out.println("Private = " + a);
+    }
+}
+
+class B extends A {
+
+    public void methB() {
+        System.out.println("\nAccessing the properties from the sub-class in the same package: ");
+        System.out.println("Public = " + x);
+        System.out.println("Protected = " + y);
+        System.out.println("Default = " + z);
+    //  System.out.println(a);
+    }
+}
+
+public class Packages {
+
+    public int x = 5;
+    protected int y = 77;
+    int z = 19;
+    private int a = 6;
+
+    public static void main(String[] args) {
+
+        /* Modifiers	Class	Package	  Sub-class	  World
+		   
+		   Public		  Y			Y		 Y			Y
+		   Protected	  Y			Y		 Y			N
+		   Default		  Y			Y		 N			N
+		   Private		  Y    		N		 N			N		 */
+
+        A a = new A();
+        a.methA();
+
+        B b = new B();
+        b.methB();
+
+        System.out.println("\nAccessing the properties of a class form another class but in the same package:  ");
+        System.out.println("Public = " + a.x); // x is Public
+        System.out.println("Protected = " + a.y); // y is Protected
+        System.out.println("Default = " + a.z); // z is Default
+        // System.out.println(a.a); // a is Private
+
+    }
+}
---
+++ Packages_02.java
@@ -0,0 +1,42 @@
+package com2.company2;
+
+import com.company.Packages;
+
+class C extends Packages {
+
+    /* class 'C' inherits class 'Packages' which is in other package.
+       So, 'C' can access only Public and Protected methods/properties of 'Packages' and not Default & Private.
+     */
+
+    public void me2() {
+        System.out.println("\nAccessing the properties from sub-class but from another package:");
+		System.out.println("Public = " +x); // x is Public
+		System.out.println("Protected = " +y); // y is Protected
+	//	System.out.println(z); // z is Default
+	//	System.out.println(a); // a is Private
+	}
+}
+
+public class Packages_02 {
+    public static void main(String[] args) {
+
+        /* -class 'Packages' & class 'Packages_02' are both in different packages.
+           -I've imported Packages.class here in this code.
+           -And I've made an object of that class.
+           -If I try and access the properties/methods of 'Packages' here in 'Packages_02',
+            I can access only Public properties/methods of 'Packages'.
+           -And I cannot access Protected, Default & Private properties/methods of 'Packages'
+            here in 'Packages_02'.
+         */
+
+        Packages a2 = new Packages();
+        System.out.println("\nAccessing the properties from Java world i.e. form another package:");
+        System.out.println("Public = " + a2.x); // x is Public
+    //  System.out.println(a2.y); // y is Protected
+    //  System.out.println(a2.z); // z is Default
+    //  System.out.println(a2.a); // a is Private
+
+        C c = new C();
+        c.me2();
+    }
+}
